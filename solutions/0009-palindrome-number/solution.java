ce22526c368ad258d29ae26ee7984de736463d81
Prashant kumar <152008521+prashantatgithub761@users.noreply.github.com>
2026-06-09 17:39:53 +0000

    [LeetCode Sync] Runtime - 4 ms (100.00%), Memory - 45.9 MB (72.15%)

--- solutions/0009-palindrome-number/solution.java
+++ solutions/0009-palindrome-number/solution.java
@@ -1,22 +1,13 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x<0){
-            return false;
-        }
-        //find rev
-        int d = 0;
         int rev = 0;
-        int dup = x;
-        while(dup>0){
-            d = dup%10;
-            dup=dup/10;
-            rev = rev*10+d;
+        int temp = x;
+        while(temp>0){
+            int lastDigit = temp%10;
+            rev=rev*10+lastDigit;
+            temp = temp/10;
+
         }
-        if(rev==x){
-            return true;
-        }
-        return false;
-        
-        
+        return x==rev;
     }
 }
