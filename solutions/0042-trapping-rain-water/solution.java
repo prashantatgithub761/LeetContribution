658dddc9fbdf9d95243ed5335104d4c275559d0d
Prashant kumar <152008521+prashantatgithub761@users.noreply.github.com>
2026-06-11 19:30:39 +0000

    [LeetCode Sync] Runtime - 1 ms (58.40%), Memory - 48.7 MB (21.92%)

--- solutions/0042-trapping-rain-water/solution.java
+++ solutions/0042-trapping-rain-water/solution.java
@@ -1,30 +1,26 @@
 class Solution {
-
     public int trap(int[] height) {
-
         int n = height.length;
 
-        int[] leftMax = new int[n];
-        int[] rightMax = new int[n];
+        int[] Leftmax = new int[n];
+        int[] Rightmax = new int[n];
 
-        leftMax[0] = height[0];
-
+        Leftmax[0] = height[0];
         for(int i = 1; i < n; i++) {
-            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+            Leftmax[i] = Math.max(Leftmax[i - 1], height[i]);
         }
 
-        rightMax[n - 1] = height[n - 1];
-
+        Rightmax[n - 1] = height[n - 1];
         for(int i = n - 2; i >= 0; i--) {
-            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+            Rightmax[i] = Math.max(Rightmax[i + 1], height[i]);
         }
 
-        int water = 0;
+        int waterLevel = 0;
 
         for(int i = 0; i < n; i++) {
-            water += Math.min(leftMax[i], rightMax[i]) - height[i];
+            waterLevel += Math.min(Leftmax[i], Rightmax[i]) - height[i];
         }
 
-        return water;
+        return waterLevel;
     }
 }
