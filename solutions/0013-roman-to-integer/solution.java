4c8aa478f84631064b3048d1f8259d29197b39c3
Prashant kumar <152008521+prashantatgithub761@users.noreply.github.com>
2026-07-08 16:48:33 +0000

    [LeetCode Sync] Runtime - 4 ms (53.67%), Memory - 46.8 MB (20.49%)

---
+++ solutions/0013-roman-to-integer/solution.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+
+class Solution {
+    public int romanToInt(String s) {
+        int sum = 0;
+        
+        
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int prevValue = 0;
+        
+        
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int currentValue = map.get(s.charAt(i));
+            
+           
+            if (currentValue < prevValue) {
+                sum -= currentValue;
+            } else {
+                
+                sum += currentValue;
+            }
+            
+            
+            prevValue = currentValue;
+        }
+        
+        return sum;
+    }
+}
