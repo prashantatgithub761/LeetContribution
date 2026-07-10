12e2822b86fa6fa3fdc4da0ef21e2ea2bc9133a4
Prashant kumar <152008521+prashantatgithub761@users.noreply.github.com>
2026-07-10 15:53:19 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 48.3 MB (69.85%)

--- solutions/0792-binary-search/solution.java
+++ solutions/0792-binary-search/solution.java
@@ -1,25 +1,20 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int n = nums.length;
-        int right = n-1;
         int left = 0;
-        while(right>=left){
+        int right = nums.length-1;
+        
+        while(left<=right){
             int mid = left+(right-left)/2;
-            if(target==nums[mid]){
+            if(nums[mid]==target){
                 return mid;
             }
-            else if (target<nums[mid]){
-                // move toward left
-                right=mid-1;
-                
-
+            else if(nums[mid]>target){
+                right = mid-1;
             }
             else{
                 left = mid+1;
-
             }
         }
         return -1;
-        
     }
 }
