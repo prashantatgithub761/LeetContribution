3eaaa22f94a3c34e74508feb8cb588f5dc5cfcd5
Prashant kumar <152008521+prashantatgithub761@users.noreply.github.com>
2026-07-12 17:43:31 +0000

    [LeetCode Sync] Runtime - 15 ms (24.47%), Memory - 52.6 MB (85.65%)

--- solutions/0169-majority-element/solution.java
+++ solutions/0169-majority-element/solution.java
@@ -1,20 +1,33 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int count = 0;
-        int candidate = 0;
+        int n = nums.length;
+        // create a hashmap to store count of every element 
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int num : nums){
+            
+            if(map.containsKey(num)){
+                // increate the rank 
+                map.put(num,map.get(num)+1);
 
-        for (int num : nums) {
-            if (count == 0) {
-                candidate = num;
             }
-
-            if (num == candidate) {
-                count++;
-            } else {
-                count--;
+            else {
+                // count of i is 1
+                map.put(num,1);
             }
         }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+           if (entry.getValue() > n / 2) {
+              return entry.getKey();
+            }
+        }
+       
+        // and then traverse through the hashmap if any element have count>mid/2
+        // return that element
+        return -1;
 
-        return candidate;
+
+
+
+        
     }
 }
