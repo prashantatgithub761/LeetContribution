f172ea9e3c35b53f851285b0279e7bc592f5033b
Prashant kumar <152008521+prashantatgithub761@users.noreply.github.com>
2026-07-12 05:50:17 +0000

    [LeetCode Sync] Runtime - 34 ms (59.96%), Memory - 59.2 MB (59.98%)

--- solutions/0015-3sum/solution.java
+++ solutions/0015-3sum/solution.java
@@ -3,14 +3,16 @@ import java.util.*;
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
 
-        List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
 
         for (int i = 0; i < nums.length - 2; i++) {
 
-            // Skip duplicate first elements
-            if (i > 0 && nums[i] == nums[i - 1])
+            if (i > 0 && nums[i] == nums[i - 1]){
                 continue;
+
+            }
+                
 
             int left = i + 1;
             int right = nums.length - 1;
@@ -26,11 +28,9 @@ class Solution {
                     left++;
                     right--;
 
-                    // Skip duplicate left values
                     while (left < right && nums[left] == nums[left - 1])
                         left++;
 
-                    // Skip duplicate right values
                     while (left < right && nums[right] == nums[right + 1])
                         right--;
 
