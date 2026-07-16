818532f3d8ffeb75759a82eafabe2749b2fcc5a0
Prashant kumar <152008521+prashantatgithub761@users.noreply.github.com>
2026-07-16 18:10:36 +0000

    [LeetCode Sync] Runtime - 1 ms (99.89%), Memory - 46.8 MB (89.40%)

--- solutions/2032-largest-odd-number-in-string/solution.java
+++ solutions/2032-largest-odd-number-in-string/solution.java
@@ -1,15 +1,15 @@
 class Solution {
     public String largestOddNumber(String num) {
-        // Scan from the end of the string to the beginning
+
         for (int i = num.length() - 1; i >= 0; i--) {
-            // Check if the current character is an odd digit
-            if ((num.charAt(i) - '0') % 2 != 0) {
-                // Return the substring from the start to this odd digit
+
+            int digit = num.charAt(i) - '0';
+
+            if (digit % 2 != 0) {
                 return num.substring(0, i + 1);
             }
         }
-        // If no odd digit is found, return an empty string
+
         return "";
     }
 }
-
