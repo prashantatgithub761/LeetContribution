e5f8f5856d4bef3837dde303aeb3100cbf06d48e
Prashant kumar <152008521+prashantatgithub761@users.noreply.github.com>
2026-07-18 11:58:51 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44 MB (30.47%)

--- solutions/0088-merge-sorted-array/solution.java
+++ solutions/0088-merge-sorted-array/solution.java
@@ -1,34 +1,23 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-
-        int[] arr = new int[m + n];
-
-        int a = 0;
-        int b = 0;
-        int c = 0;
-
-        // Compare elements while both arrays have elements
-        while (a < m && b < n) {
-            if (nums1[a] <= nums2[b]) {
-                arr[c++] = nums1[a++];
-            } else {
-                arr[c++] = nums2[b++];
+        int p = m-1;
+        int q = n-1;
+        
+        
+        for(int i = m+n-1;i>=0;i--){
+            if(q<0){
+                break;
             }
-        }
+            
 
-        // Copy remaining elements of nums1
-        while (a < m) {
-            arr[c++] = nums1[a++];
-        }
-
-        // Copy remaining elements of nums2
-        while (b < n) {
-            arr[c++] = nums2[b++];
-        }
-
-        // Copy merged array back to nums1
-        for (int i = 0; i < m + n; i++) {
-            nums1[i] = arr[i];
+            if(p>=0 && nums1[p]>=nums2[q]){
+                nums1[i]=nums1[p--];
+            }
+            else{
+                nums1[i]=nums2[q--];
+            }
+            
+            
         }
     }
 }
