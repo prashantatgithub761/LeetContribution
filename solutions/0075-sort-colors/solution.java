ff862aab50f317d3a926ce75ce932c80099ee136
Prashant kumar <152008521+prashantatgithub761@users.noreply.github.com>
2026-07-24 06:16:34 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.3 MB (89.59%)

--- solutions/0075-sort-colors/solution.java
+++ solutions/0075-sort-colors/solution.java
@@ -1,44 +1,66 @@
 class Solution {
     public void sortColors(int[] nums) {
         // count number of zero , one and two
-        int zero = 0;
-        int one = 0;
-        int two = 0;
-        for(int i = 0;i<nums.length;i++){
-            if(nums[i]==0){
-                zero++;
+        // int zero = 0;
+        // int one = 0;
+        // int two = 0;
+        // for(int i = 0;i<nums.length;i++){
+        //     if(nums[i]==0){
+        //         zero++;
 
-            }
-            else if (nums[i]==1){
-                one++;
+        //     }
+        //     else if (nums[i]==1){
+        //         one++;
+
+        //     }
+        //     else if(nums[i]==2){
+        //         two++;
+        //     }
+        // }
+        // // fill in the array
+        // int count =0;
+        // for(int i = 0;i<zero;i++){
+        //     nums[count++] = 0;
+        // }
+        // for(int i = 0;i<one;i++){
+        //     nums[count++] = 1;
+        // }
+        // for(int i = 0;i<two;i++){
+        //     nums[count++] = 2;
+        // }
+        
+        //Used dutch NationalFlag
+
+        int low = 0;
+        int high = nums.length-1;
+        int i = 0;
+
+        while(i<=high){
+            if(nums[i]==0){
+                // swap with low
+                int temp = nums[low];
+                nums[low] = nums[i];
+                nums[i]=temp;
+                low++;
+                i++;
 
             }
             else if(nums[i]==2){
-                two++;
+                // put the 2 in last
+                int temp = nums[i];
+                nums[i]=nums[high];
+                nums[high]=temp;
+                high--;
+                
             }
-        }
-        // fill in the array
-        int count =0;
-        for(int i = 0;i<zero;i++){
-            nums[count++] = 0;
-        }
-        for(int i = 0;i<one;i++){
-            nums[count++] = 1;
-        }
-        for(int i = 0;i<two;i++){
-            nums[count++] = 2;
+            else{
+                // put in middle
+                i++;
+
+            }
+
         }
         
-        // for(int i = 0;i<nums.length;i++){
-        //     if(nums[i]==2){
-        //         nums[i]=two;
-        //     }
-        //     else if(nums[i]==1){
-        //         nums[i]=one;
-        //     }
-        //     else if(nums[i]==0){
-        //         nums[i]=zero;
-        //     }
-        // }
+        
     }
 }
