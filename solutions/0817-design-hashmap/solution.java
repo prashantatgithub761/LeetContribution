180ea02c4a228f429a8a74af3239506c9d0f4d3a
Prashant kumar <152008521+prashantatgithub761@users.noreply.github.com>
2026-07-25 10:23:02 +0000

    [LeetCode Sync] Runtime - 15 ms (94.58%), Memory - 47.9 MB (96.74%)

--- solutions/0817-design-hashmap/solution.java
+++ solutions/0817-design-hashmap/solution.java
@@ -1,21 +1,74 @@
 class MyHashMap {
 
-    int[] map;
+    class Node {
+        int key, value;
+        Node next;
+
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private final int SIZE = 1009;
+    private Node[] buckets;
 
     public MyHashMap() {
-        map = new int[1000001];
-        Arrays.fill(map, -1);
+        buckets = new Node[SIZE];
+    }
+
+    private int hash(int key) {
+        return key % SIZE;
     }
 
     public void put(int key, int value) {
-        map[key] = value;
+        int index = hash(key);
+
+        Node curr = buckets[index];
+
+        while (curr != null) {
+            if (curr.key == key) {
+                curr.value = value;
+                return;
+            }
+            curr = curr.next;
+        }
+
+        Node node = new Node(key, value);
+        node.next = buckets[index];
+        buckets[index] = node;
     }
 
     public int get(int key) {
-        return map[key];
+        int index = hash(key);
+
+        Node curr = buckets[index];
+
+        while (curr != null) {
+            if (curr.key == key)
+                return curr.value;
+            curr = curr.next;
+        }
+
+        return -1;
     }
 
     public void remove(int key) {
-        map[key] = -1;
+        int index = hash(key);
+
+        Node curr = buckets[index];
+        Node prev = null;
+
+        while (curr != null) {
+            if (curr.key == key) {
+                if (prev == null)
+                    buckets[index] = curr.next;
+                else
+                    prev.next = curr.next;
+                return;
+            }
+            prev = curr;
+            curr = curr.next;
+        }
     }
 }
