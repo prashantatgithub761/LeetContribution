0fafdfe1286a456a62dfe6eebf9fb46abaa9301a
Prashant kumar <152008521+prashantatgithub761@users.noreply.github.com>
2026-07-25 10:50:39 +0000

    [LeetCode Sync] Runtime - 20 ms (38.86%), Memory - 107.2 MB (67.42%)

--- solutions/0217-contains-duplicate/solution.java
+++ solutions/0217-contains-duplicate/solution.java
@@ -1,13 +1,59 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> a= new HashSet<>();
-        for(int x : nums){
-            if(a.contains(x)){
+
+        // for(int i = 0;i<nums.length-1;i++){
+        //     for(int j = i+1;j<nums.length;j++){
+        //         if(nums[i]==nums[j]){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+
+        // HashSet<Integer> a = new HashSet<>();
+        // for(int i : nums){
+        //     if(a.contains(i)){
+        //         return true;
+        //     }
+        //     a.add(i);
+        // }
+        // return false;
+
+
+        // Arrays.sort(nums);
+        // for(int i = 0;i<nums.length-1;i++){
+        //     if(nums[i]==nums[i+1]){
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+       
+        // Initialize with nums.length to avoid background resizing
+        HashSet<Integer> seen = new HashSet<>(nums.length);
+        
+        for (int num : nums) {
+            // If the set already has the number, it's a duplicate
+            if (seen.contains(num)) {
                 return true;
             }
-            a.add(x);
-
+            seen.add(num);
         }
         return false;
+    
+
+
+
+
+
+        // HashSet<Integer> a= new HashSet<>();
+        // for(int x : nums){
+        //     if(a.contains(x)){
+        //         return true;
+        //     }
+        //     a.add(x);
+
+        // }
+        // return false;
     }
 }
