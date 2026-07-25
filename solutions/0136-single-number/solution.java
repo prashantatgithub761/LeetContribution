f09e90e8e56ff1fd92d76f36cc1a0eff1071fc17
Prashant kumar <152008521+prashantatgithub761@users.noreply.github.com>
2026-07-25 10:57:01 +0000

    [LeetCode Sync] Runtime - 1 ms (99.94%), Memory - 46.8 MB (65.40%)

--- solutions/0136-single-number/solution.java
+++ solutions/0136-single-number/solution.java
@@ -1,9 +1,8 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        // could use hashmap by counting count nd if count is one return it otherwise -1
         int ans = 0;
-        for(int x : nums){
-            ans ^= x;
+        for(int i : nums){
+            ans^=i;
 
         }
         return ans;
