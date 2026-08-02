f359e4825937c8fbd00e75264ff5576f2ab07ef4
Prashant kumar <152008521+prashantatgithub761@users.noreply.github.com>
2026-08-02 03:16:46 +0000

    [LeetCode Sync] Runtime - 7 ms (21.50%), Memory - 45.4 MB (9.45%)

---
+++ solutions/1510-find-lucky-integer-in-an-array/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findLucky(int[] arr) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int i = 0;i<arr.length;i++){
+            if(map.containsKey(arr[i])){
+                map.put(arr[i],map.get(arr[i])+1);
+            }
+            else{
+                map.put(arr[i],1);
+            }
+        }
+        int ans = -1;
+        for(int key:map.keySet()){
+            if(key==map.get(key)){
+                ans = Math.max(ans,key);
+            }
+        }
+        return ans;
+    }
+}
