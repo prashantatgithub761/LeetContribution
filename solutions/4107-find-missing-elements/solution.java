c8c46eb30c43be4bf538d7f52ca65371037552db
Prashant kumar <152008521+prashantatgithub761@users.noreply.github.com>
2026-08-04 16:40:52 +0000

    [LeetCode Sync] Runtime - 1 ms (100.00%), Memory - 46.8 MB (55.30%)

---
+++ solutions/4107-find-missing-elements/solution.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        boolean[] present = new boolean[101];
+
+        for (int num : nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+            present[num] = true;
+        }
+
+        List<Integer> ans = new ArrayList<>();
+
+        for (int i = min; i <= max; i++) {
+            if (!present[i]) {
+                ans.add(i);
+            }
+        }
+
+        return ans;
+    }
+}
