4f0c6bfd41bfa5b5bd4b7988d9e8edf169e70134
Prashant kumar <152008521+prashantatgithub761@users.noreply.github.com>
2026-08-10 11:51:33 +0000

    [LeetCode Sync] Runtime - 33 ms (72.27%), Memory - 59.2 MB (45.47%)

--- solutions/0015-3sum/solution.java
+++ solutions/0015-3sum/solution.java
@@ -2,46 +2,53 @@ import java.util.*;
 
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-
         Arrays.sort(nums);
+        int n = nums.length;
+        
         List<List<Integer>> ans = new ArrayList<>();
 
-        for (int i = 0; i < nums.length - 2; i++) {
-
+        
+        for(int i = 0;i<n;i++){
             if (i > 0 && nums[i] == nums[i - 1]){
-                continue;
-
+             continue;
             }
-                
+            int target = -nums[i];
 
-            int left = i + 1;
-            int right = nums.length - 1;
-
-            while (left < right) {
-
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (sum == 0) {
-
-                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
-
+            // now find two digits who's summ is -ve of digit
+            int left = i+1;
+            int right = n-1;
+            while(left<right){
+                int sum = nums[left]+nums[right];
+                if(sum == target){
+                    ans.add(Arrays.asList(nums[i],nums[right],nums[left]));
                     left++;
                     right--;
-
-                    while (left < right && nums[left] == nums[left - 1])
+                    while (left < right && nums[left] == nums[left - 1]){
                         left++;
 
-                    while (left < right && nums[right] == nums[right + 1])
-                        right--;
+                    }
+                      
 
-                } else if (sum < 0) {
-                    left++;
-                } else {
+                    while (left < right && nums[right] == nums[right + 1]){
+                        right--;
+                    }
+                      
+                    
+                }
+                else if(sum>target){
                     right--;
                 }
+                else{
+                    left++;
+                }
             }
-        }
+            
+        
 
+        }
         return ans;
+       
+        
+        
     }
 }
