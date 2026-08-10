0bb72742c0efe91f3f90e6adac8a1c417e6853e4
Prashant kumar <152008521+prashantatgithub761@users.noreply.github.com>
2026-08-10 13:00:26 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.6 MB (24.02%)

--- solutions/0075-sort-colors/solution.java
+++ solutions/0075-sort-colors/solution.java
@@ -31,36 +31,77 @@ class Solution {
         
         //Used dutch NationalFlag
 
-        int low = 0;
-        int high = nums.length-1;
-        int i = 0;
+        // int low = 0;
+        // int high = nums.length-1;
+        // int i = 0;
 
-        while(i<=high){
-            if(nums[i]==0){
-                // swap with low
-                int temp = nums[low];
-                nums[low] = nums[i];
-                nums[i]=temp;
-                low++;
-                i++;
+        // while(i<=high){
+        //     if(nums[i]==0){
+        //         // swap with low
+        //         int temp = nums[low];
+        //         nums[low] = nums[i];
+        //         nums[i]=temp;
+        //         low++;
+        //         i++;
 
-            }
-            else if(nums[i]==2){
-                // put the 2 in last
-                int temp = nums[i];
-                nums[i]=nums[high];
-                nums[high]=temp;
-                high--;
+        //     }
+        //     else if(nums[i]==2){
+        //         // put the 2 in last
+        //         int temp = nums[i];
+        //         nums[i]=nums[high];
+        //         nums[high]=temp;
+        //         high--;
+                
+        //     }
+        //     else{
+        //         // put in middle
+        //         i++;
+
+        //     }
+
+
+            int i = 0;
+            int j = nums.length-1;
+            int a = 0;
+
+            while(a<=j){
+                if(nums[a]==0){
+                    // put in first
+                    int temp = nums[i];
+                    nums[i]=nums[a];
+                    nums[a] = temp;
+                    i++;
+                    a++;
+                }
+                else if(nums[a]==2){
+                    int temp = nums[j];
+                    nums[j] = nums[a];
+                    nums[a]=temp;
+                    j--;
+                }
+                else{
+                    a++;
+                }
                 
             }
-            else{
-                // put in middle
-                i++;
+            
 
-            }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         }
         
         
     }
-}
+
