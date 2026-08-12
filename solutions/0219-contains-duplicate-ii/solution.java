8be3ae07b5768b7a1c45259dec4d8ecd1a3949cc
Prashant kumar <152008521+prashantatgithub761@users.noreply.github.com>
2026-08-12 08:40:30 +0000

    [LeetCode Sync] Runtime - 28 ms (51.06%), Memory - 92 MB (60.71%)

--- solutions/0219-contains-duplicate-ii/solution.java
+++ solutions/0219-contains-duplicate-ii/solution.java
@@ -2,18 +2,24 @@ import java.util.*;
 
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
+     // Adds the element of that window into the hashtable and if that window's element contains duplicates value so return true;
+     // else remove the prev. element and add new one and know check again..
+     HashSet<Integer> set = new HashSet<>();
+     for(int i = 0;i<nums.length;i++){
+        if(set.contains(nums[i])){
+            return true;
 
-        HashMap<Integer, Integer> map = new HashMap<>();
+        }
+        set.add(nums[i]);
+        // Maintain Set Size
 
-        for (int i = 0; i < nums.length; i++) {
-
-            if (map.containsKey(nums[i]) &&
-                i - map.get(nums[i]) <= k)
-                return true;
-
-            map.put(nums[i], i);
+        if(set.size()>k){
+            set.remove(nums[i-k]);
         }
 
-        return false;
+
+     }
+     return false;
+
     }
 }
