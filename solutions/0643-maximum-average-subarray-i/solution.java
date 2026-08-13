3f8cb1ee902b16ecfd9e52c469c61b7cc226c199
Prashant kumar <152008521+prashantatgithub761@users.noreply.github.com>
2026-08-13 16:31:52 +0000

    [LeetCode Sync] Runtime - 2 ms (99.79%), Memory - 69.6 MB (63.36%)

--- solutions/0643-maximum-average-subarray-i/solution.java
+++ solutions/0643-maximum-average-subarray-i/solution.java
@@ -1,22 +1,48 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-        int left = 0;
-        int right = k-1;
+        // int left = 0;
+        // int right = k-1;
         
+        // int sum = 0;
+        // for(int i = left;i<=right;i++){
+        //     sum+=nums[i];
+
+        // }
+        
+        // // now find to max avg
+        // int MaxSum = sum;
+        // while(right<nums.length-1){
+        //     sum = sum+nums[++right]-nums[left++];
+            
+        //     MaxSum = Math.max(MaxSum,sum);
+        // }
+        // return (double)MaxSum/k;
+
+
+        // find sum of first window
         int sum = 0;
-        for(int i = left;i<=right;i++){
+        
+        for(int i = 0;i<k;i++){
             sum+=nums[i];
+            
+
+        }
+
+        // now traverse into the all windows and update max 
+        int i = 0;
+        
+        int max = sum;
+
+        for(int j = k;j<nums.length;j++){
+            sum+=nums[j]-nums[i];
+            i++;
+            max = Math.max(max,sum);
 
         }
         
-        // now find to max avg
-        int MaxSum = sum;
-        while(right<nums.length-1){
-            sum = sum+nums[++right]-nums[left++];
-            
-            MaxSum = Math.max(MaxSum,sum);
-        }
-        return (double)MaxSum/k;
+        
+        return (double)max/k;
+
         
     }
 }
