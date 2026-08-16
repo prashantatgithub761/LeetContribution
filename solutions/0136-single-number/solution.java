cd97aa9866b504317ecf7c1543589c653a077aed
Prashant kumar <152008521+prashantatgithub761@users.noreply.github.com>
2026-08-16 10:52:32 +0000

    [LeetCode Sync] Runtime - 1 ms (99.94%), Memory - 46.8 MB (65.40%)

--- solutions/0136-single-number/solution.java
+++ solutions/0136-single-number/solution.java
@@ -1,10 +1,11 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int ans = 0;
-        for(int i : nums){
+        for(int i:nums){
             ans^=i;
 
         }
         return ans;
+       
     }
 }
