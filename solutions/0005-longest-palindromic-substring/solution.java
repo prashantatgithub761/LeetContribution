ab71bd5f949fca4062ed4e758490fa66293b2d82
Prashant kumar <152008521+prashantatgithub761@users.noreply.github.com>
2026-09-07 15:44:24 +0000

    [LeetCode Sync] Runtime - 13 ms (96.46%), Memory - 43.3 MB (95.05%)

--- solutions/0005-longest-palindromic-substring/solution.java
+++ solutions/0005-longest-palindromic-substring/solution.java
@@ -1,42 +1,30 @@
 class Solution {
+    private int start = 0;
+    private int maxLength = 1;
 
     public String longestPalindrome(String s) {
-
-        if (s == null || s.length() < 2)
-            return s;
-
-        int start = 0;
-        int end = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-
-            // Odd length palindrome
-            int len1 = expand(s, i, i);
-
-            // Even length palindrome
-            int len2 = expand(s, i, i + 1);
-
-            int len = Math.max(len1, len2);
-
-            if (len > end - start + 1) {
-                start = i - (len - 1) / 2;
-                end = i + len / 2;
-            }
+        for (int center = 0; center < s.length(); center++) {
+            expand(s, center, center);       // Odd-length palindrome
+            expand(s, center, center + 1);   // Even-length palindrome
         }
 
-        return s.substring(start, end + 1);
+        return s.substring(start, start + maxLength);
     }
 
-    private int expand(String s, int left, int right) {
-
+    private void expand(String s, int left, int right) {
         while (left >= 0 &&
                right < s.length() &&
                s.charAt(left) == s.charAt(right)) {
-
             left--;
             right++;
         }
 
-        return right - left - 1;
+        // left and right are now one position beyond the palindrome.
+        int length = right - left - 1;
+
+        if (length > maxLength) {
+            maxLength = length;
+            start = left + 1;
+        }
     }
 }
