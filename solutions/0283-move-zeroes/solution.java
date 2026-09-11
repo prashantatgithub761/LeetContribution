e0fcc7fe022a19a19ef9f49ede6ade079b5b4aa8
Prashant kumar <152008521+prashantatgithub761@users.noreply.github.com>
2026-09-11 10:04:54 +0000

    [LeetCode Sync] Runtime - 2 ms (91.76%), Memory - 47.9 MB (20.17%)

--- solutions/0283-move-zeroes/solution.java
+++ solutions/0283-move-zeroes/solution.java
@@ -1,16 +1,46 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int left = 0;
-        for(int right = 0;right<nums.length;right++){
-            if(nums[right]!=0){
-                // swap the values;
-                int temp = nums[right];
-                nums[right] = nums[left];
-                nums[left] = temp;
-                left++;
+        // int n = nums.length;
+        // int zeroes = 0;
+        // int a = 0;
+        // for(int i = 0;i<n;i++){
+        //     // count zeros
+        //     if(nums[i]==0){
+        //         zeroes++;
 
+        //     }
+        //     else{
+        //         nums[a++]=nums[i];
+
+        //     }
+
+            
+        // }
+        // // add the rest of elements as zero in last
+        // for(int i = n-1;i>=n-zeroes;i--){
+        //     nums[i]=0;
+        // }
+
+
+        // count the number of zeroes and add them at last
+        int index = 0;
+        int zero = 0;
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i]!=0){
+                nums[index]=nums[i];
+                index++;
             }
-
+            else{
+                zero++;
+            }
+            
+        }
+        
+        // now fill zeroes from last
+        while(zero>0){
+            nums[index] = 0;
+            index++;
+            zero--;
         }
         
     }
