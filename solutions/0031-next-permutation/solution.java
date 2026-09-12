00454a1ac1ddb38dd33eccaee19cebf42443ff6d
Prashant kumar <152008521+prashantatgithub761@users.noreply.github.com>
2026-09-12 03:32:47 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.9 MB (15.68%)

--- solutions/0031-next-permutation/solution.java
+++ solutions/0031-next-permutation/solution.java
@@ -1,43 +1,123 @@
 class Solution {
     public void nextPermutation(int[] nums) {
 
+        // 1. find the breakThrough
         int n = nums.length;
+        int i = -1;
 
-        // Step 1: Find the pivot
-        int i = n - 2;
-
-        while (i >= 0 && nums[i] >= nums[i + 1]) {
-            i--;
-        }
-
-        // Step 2: If pivot exists, find the next greater element
-        if (i >= 0) {
-
-            int j = n - 1;
-
-            while (nums[j] <= nums[i]) {
-                j--;
+        for(int ind = n-2;ind>=0;ind--){
+            if(nums[ind]<nums[ind+1]){
+                i=ind;
+                break;
             }
-
-            swap(nums, i, j);
+        }
+        if(i==-1){
+            reverse(nums,0,n-1);
+            return;
         }
 
-        // Step 3: Reverse the suffix
-        reverse(nums, i + 1, n - 1);
+        // now find the min element from right of i that is > than element at i 
+        int min = 101;
+        int k = 0;
+
+        for(int j = i+1;j<n;j++){
+            if(nums[j]>nums[i]){
+                min = Math.min(min,nums[j]);
+                k = j;
+                
+                // now i have breakpoint and the element to which i have to swap
+
+            }
+        }
+        // swap the elements
+        int temp = nums[k];
+        nums[k] = nums[i];
+        nums[i] = temp;
+
+        // now sort the element after i
+        reverse(nums,i+1,n-1);
+
+
+
+
+
+
     }
 
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
 
-    private void reverse(int[] nums, int left, int right) {
-
-        while (left < right) {
-            swap(nums, left, right);
-            left++;
-            right--;
+    void reverse(int[] nums, int a, int b){
+        while(a<b){
+            int temp = nums[a];
+            nums[a] = nums[b];
+            nums[b] = temp;
+            a++;
+            b--;
         }
     }
+        // 1. Find the first smaller number from the right
+
+
+        // int i = n-2;
+        // while(i>=0&&nums[i]>=nums[i+1]){
+        //     i--;
+        // }
+
+
+
+
+
+
+
+
+
+
+
+
+    //     int i = nums.length - 2;
+
+    //     while (i >= 0 && nums[i] >= nums[i + 1]) {
+    //         i--;
+    //     }
+
+
+    //     // 2. Swap it with the first larger number from the right
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //     if (i >= 0) {
+    //         int j = nums.length - 1;
+
+    //         while (nums[j] <= nums[i]) {
+    //             j--;
+    //         }
+
+    //         swap(nums, i, j);
+    //     }
+
+    //     // 3. Reverse the part after i
+    //     int left = i + 1;
+    //     int right = nums.length - 1;
+
+    //     while (left < right) {
+    //         swap(nums, left, right);
+    //         left++;
+    //         right--;
+    //     }
+    
+
+    // void swap(int[] nums, int a, int b) {
+    //     int temp = nums[a];
+    //     nums[a] = nums[b];
+    //     nums[b] = temp;
+    // }
 }
