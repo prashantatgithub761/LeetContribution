f9dabeab4dd21087c0ac36641d2c83317f1a5d30
Prashant kumar <152008521+prashantatgithub761@users.noreply.github.com>
2026-09-18 15:20:25 +0000

    [LeetCode Sync] Runtime - 4 ms (36.39%), Memory - 43.4 MB (40.83%)

--- solutions/0020-valid-parentheses/solution.java
+++ solutions/0020-valid-parentheses/solution.java
@@ -1,28 +1,64 @@
 class Solution {
     public boolean isValid(String s) {
+        // Stack<Character> St = new Stack<>();
+        // for(char ch: s.toCharArray()){
+        //     if(ch=='('||ch=='{'||ch=='['){
+        //         St.push(ch);
+        //     }
+        //     else if(ch==')' && !St.isEmpty() && St.peek()=='('){
+        //         St.pop();
+
+        //     }
+        //     else if(ch==']' && !St.isEmpty() && St.peek()=='['){
+        //         St.pop();
+
+        //     }
+        //     else if(ch=='}' && !St.isEmpty() && St.peek()=='{'){
+        //         St.pop();
+
+        //     }
+        //     else{
+        //         return false;
+        //     }
+        // }
+
+        // return St.isEmpty();
+
+/*
+        // Make a Stack 
+        // Push if u get oppening 
+        pop is closing and in last if the stack is open return true 
+        else false
+
+        
+        
+        */
+
         Stack<Character> St = new Stack<>();
-        for(char ch: s.toCharArray()){
-            if(ch=='('||ch=='{'||ch=='['){
-                St.push(ch);
+
+        for(char c: s.toCharArray()){
+            if(c == '(' || c=='{' || c=='['){
+                St.push(c);
             }
-            else if(ch==')' && !St.isEmpty() && St.peek()=='('){
+            else if(c==')' && !St.isEmpty() && St.peek()=='('){
                 St.pop();
 
             }
-            else if(ch==']' && !St.isEmpty() && St.peek()=='['){
+            else if(c==']' && !St.isEmpty() && St.peek()=='['){
                 St.pop();
 
             }
-            else if(ch=='}' && !St.isEmpty() && St.peek()=='{'){
+            else if(c=='}' && !St.isEmpty() && St.peek()=='{'){
                 St.pop();
 
             }
             else{
                 return false;
             }
-        }
 
+            
+        }
         return St.isEmpty();
-        
     }
 }
+
